6b7edd3565aec87c960fe2ddc7223b1a754fe182
gabady13 <gabadysergei@yandex.ru>
2019-03-25 19:49:24 +0300

    dddd

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 import askName from '..';
-import Welcome from './Welcome';
+import Welcome from '../Welcome';
 import { random } from 'lodash/fp';
 
 const readlineSync = require('readline-sync');
-
 
 const roundCount = 3;
 const roundCo = 0;
@@ -15,25 +14,34 @@ console.log('Answer "yes" if number even otherwise answer "no".');
 const userName = askName();
 console.log(`Hello, ${userName}!`);
 
+const failAnsver = (ansver, LuserName) => {
+  if (ansver === 'yes') {
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${LuserName}!`);
+  } else {
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${LuserName}!`);
+  }
+};
+
+const askQuastion = () => {
+  const rand = random(1, 100);
+  console.log(`Question:${rand}`);
+  const ansver = readlineSync.question('Your answer:');
+  const obj = {};
+  obj.numer = rand;
+  obj.ansver = ansver;
+  return obj;
+};
+
 const roundOfGames = (roundCou, countRounds, LuserName) => {
   if (roundCou === countRounds) {
     console.log(`Congratulations, ${LuserName}!`);
   } else {
-    const rand = random(1, 100);
-    console.log(`Question:${rand}`);
-    const ansver = readlineSync.question('Your answer:');
-    if (rand % 2 === 0) {
-      if (ansver === 'yes') {
-        console.log('Correct!');
-        roundOfGames(roundCou + 1, countRounds, LuserName);
-      } else {
-        console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${LuserName}!`);
-      }
-    } else if (ansver === 'no') {
-      console.log('Correct!');
+    const Quastion = askQuastion();
+    if ((Quastion.numer % 2 === 0 && Quastion.ansver === 'yes') || (Quastion.numer % 2 !== 0 && Quastion.ansver === 'no')) {
+      console.log('Correct');
       roundOfGames(roundCou + 1, countRounds, LuserName);
     } else {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${LuserName}!`);
+      failAnsver(Quastion.ansver, LuserName);
     }
   }
 };
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import askName from '..';
-import Welcome from './Welcome';
+import Welcome from '../Welcome';
 
 console.log(Welcome());
 const userName = askName();
